7d8c7627dcd0b4539419b9166fe7b3b29bde99f2
IbrahemHurani <86603326+IbrahemHurani@users.noreply.github.com>
2021-11-05 15:52:32 +0200

    Delete Ex1 directory

--- Ex1/NumClass.h
+++
@@ -1,9 +0,0 @@
-#ifndef _NumClass
-#define _NumClass
-#include<stdio.h>
-#include<math.h>
-int isPrime(int n);//return if the number is prime number
-int isStrong(int n);//return if the number is Strong number
-int isArmStrong(int n);//return if the numberis ArmStrong number
-int isPalindrome(int n);//return if the number is Palindrome
-#endif
