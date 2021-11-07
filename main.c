5dfb9a03ea180d9cb278ee3a6f852efaa1538673
IbrahemHurani <86603326+IbrahemHurani@users.noreply.github.com>
2021-11-07 10:27:57 -0500

    Add files via upload

---
+++ main.c
@@ -0,0 +1,32 @@
+#include<stdio.h>
+#include "NumClass.h"
+int main() {
+	int number1, number2;
+	scanf("%d", &number1);
+	scanf("%d", &number2);
+	int i;
+	printf("\nThe ArmStrong numbers are:\n");
+	for (i = number1; i <= number2; i++) {
+		if (isArmStrong(i) == 1)
+			printf(" %d ", i);
+	}
+	printf("\nThe Palindrome numbers are:\n");
+	for (i = number1; i <= number2; i++) {
+		if (isPalindrome(i) == 1)
+			printf(" %d ", i);
+	}
+	printf("\nThe Prime numbers are:\n");
+	for (i = number1; i <= number2; i++) {
+		if (isPrime(i) == 1)
+			printf(" %d ", i);
+	}
+	printf("\nthe Strong numbers are:\n");
+	for (i = number1; i <= number2; i++) {
+		if (isStrong(i) == 1)
+			printf(" %d ", i);
+	}
+	
+
+return 0;
+
+}
